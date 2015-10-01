c0fe1c0e89277b92ff38d174ee92722ea1a102c6
Jozef Hollý <j2.00ghz@gmail.com>
2015-10-01 13:16:24 +0200

    Add user and file class

---
+++ IntranetGJAK/Models/File.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IntranetGJAK.Models
+{
+    public class File
+    {
+        public int FileId { get; set; }
+
+        [StringLength(255)]
+        public string FileName { get; set; }
+
+        [StringLength(100)]
+        public string ContentType { get; set; }
+
+        public byte[] Content { get; set; }
+        public int PersonId { get; set; }
+        public virtual User Person { get; set; }
+    }
+}
---
+++ IntranetGJAK/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace IntranetGJAK.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public virtual ICollection<File> Files { get; set; }
+    }
+}
