7a7c9553c16e511b0eb4ac2110cf918f4ab3dab7
J2ghz <j2.00ghz@gmail.com>
2015-10-17 17:04:53 +0200

    start writing tests

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Backload.Demo.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
 
 namespace UnitTests
 {
@@ -9,6 +11,9 @@ namespace UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
         }
     }
 }
