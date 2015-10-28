8dc26f0c4cfc675778f8113e2c2273c82238d8dd
J2ghz <j2.00ghz@gmail.com>
2015-10-28 18:51:54 +0100

    Add travis, unit tests and first test

---
+++ UnitTests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using IntranetGJAK.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class Class1
+    {
+        [Fact]
+        public void Index()
+        {
+            Assert.NotNull((new HomeController()).Index());
+        }
+    }
+}
