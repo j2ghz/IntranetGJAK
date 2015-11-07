867b46db1ce1c22f58cc2194e203adf6d72bf077
J2ghz <j2.00ghz@gmail.com>
2015-11-07 17:27:52 +0100

    add upload handler

---
+++ IntranetGJAK/Controllers/UploadHandler.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Dnx.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace IntranetGJAK.Controllers
+{
+    public class UploadHandler : Controller
+    {
+        private readonly IApplicationEnvironment _appEnvironment;
+
+        public UploadHandler(IApplicationEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        private string StorageRoot
+        {
+            get { return Path.Combine(_appEnvironment.ApplicationBasePath, "/files"); }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        [HttpGet]
+        public void Delete(string id)
+        {
+            var filename = id;
+            var filePath = Path.Combine(_appEnvironment.ApplicationBasePath, "/files", filename);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        [HttpGet]
+        public void Download(string id)
+        {
+            var filename = id;
+            var filePath = Path.Combine(_appEnvironment.ApplicationBasePath, "~/Files", filename);
+
+            var context = HttpContext;
+
+            if (System.IO.File.Exists(filePath))
+            {
+                context.Response.Headers.Add("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+                context.Response.ContentType = "application/octet-stream";
+                context.Response.ClearContent();
+                context.Response.WriteFile(filePath);
+            }
+            else
+                context.Response.StatusCode = 404;
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        [HttpPost]
+        public ActionResult UploadFiles()
+        {
+            var r = new List<ViewDataUploadFilesResult>();
+
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                var statuses = new List<ViewDataUploadFilesResult>();
+                var headers = Request.Headers;
+
+                if (string.IsNullOrEmpty(headers["X-File-Name"]))
+                {
+                    UploadWholeFile(Request, statuses);
+                }
+                else
+                {
+                    UploadPartialFile(headers["X-File-Name"], Request, statuses);
+                }
+
+                JsonResult result = Json(statuses);
+                result.ContentType = new Microsoft.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+                return result;
+            }
+
+            return Json(r);
+        }
+
+        private string EncodeFile(string fileName)
+        {
+            return Convert.ToBase64String(System.IO.File.ReadAllBytes(fileName));
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        //Credit to i-e-b and his ASP.Net uploader for the bulk of the upload helper methods - https://github.com/i-e-b/jQueryFileUpload.Net
+        private void UploadPartialFile(string fileName, HttpRequestBase request, List<ViewDataUploadFilesResult> statuses)
+        {
+            if (request.Files.Count != 1) throw new HttpRequestValidationException("Attempt to upload chunked file containing more than one fragment per request");
+            var file = request.Files[0];
+            var inputStream = file.InputStream;
+
+            var fullName = Path.Combine(StorageRoot, Path.GetFileName(fileName));
+
+            using (var fs = new FileStream(fullName, FileMode.Append, FileAccess.Write))
+            {
+                var buffer = new byte[1024];
+
+                var l = inputStream.Read(buffer, 0, 1024);
+                while (l > 0)
+                {
+                    fs.Write(buffer, 0, l);
+                    l = inputStream.Read(buffer, 0, 1024);
+                }
+                fs.Flush();
+                fs.Close();
+            }
+            statuses.Add(new ViewDataUploadFilesResult()
+            {
+                name = fileName,
+                size = file.ContentLength,
+                type = file.ContentType,
+                url = "/Home/Download/" + fileName,
+                delete_url = "/Home/Delete/" + fileName,
+                thumbnail_url = @"data:image/png;base64," + EncodeFile(fullName),
+                delete_type = "GET",
+            });
+        }
+
+        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
+        //Credit to i-e-b and his ASP.Net uploader for the bulk of the upload helper methods - https://github.com/i-e-b/jQueryFileUpload.Net
+        private void UploadWholeFile(HttpRequestBase request, List<ViewDataUploadFilesResult> statuses)
+        {
+            for (int i = 0; i < request.Files.Count; i++)
+            {
+                var file = request.Files[i];
+
+                var fullPath = Path.Combine(StorageRoot, Path.GetFileName(file.FileName));
+
+                file.SaveAs(fullPath);
+
+                statuses.Add(new ViewDataUploadFilesResult()
+                {
+                    name = file.FileName,
+                    size = file.ContentLength,
+                    type = file.ContentType,
+                    url = "/Home/Download/" + file.FileName,
+                    delete_url = "/Home/Delete/" + file.FileName,
+                    thumbnail_url = @"data:image/png;base64," + EncodeFile(fullPath),
+                    delete_type = "GET",
+                });
+            }
+        }
+    }
+
+    public class ViewDataUploadFilesResult
+    {
+        public string name { get; set; }
+        public int size { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+        public string delete_url { get; set; }
+        public string thumbnail_url { get; set; }
+        public string delete_type { get; set; }
+    }
+}
