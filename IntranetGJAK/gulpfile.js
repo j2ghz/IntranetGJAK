6682a0c8a3b78ab302549343633a2a1188d3d60f
Jozef Hollý <j2.00ghz@gmail.com>
2015-11-27 21:02:35 +0100

    finalizing gulpfile

--- IntranetGJAK/gulpfile.js
+++ IntranetGJAK/gulpfile.js
@@ -1,5 +1,7 @@
 ﻿/// <binding BeforeBuild='default' Clean='clean' />
 /// <reference path="wwwroot/ts/tsd.d.ts" />
+/// <reference path="wwwroot/ts/_references.js" />
+
 "use strict";
 
 var gulp = require("gulp"),
@@ -18,69 +20,79 @@ var paths = {
 };
 
 paths.ts = paths.webroot + "ts/**/*.ts";
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.sass = paths.webroot + "sass/**/*.scss";
+paths.js = paths.webroot + "js/";
+paths.jsOut = paths.webroot + "js/site.js";
+paths.css = paths.webroot + "css/";
+paths.cssOut = paths.webroot + "css/site.css";
 
-gulp.task("clean:js", function (cb) {
-    rimraf(paths.webroot + "js/", cb);
+gulp.task("clean:js", function(cb) {
+    rimraf(paths.js, cb);
 });
 
-gulp.task("clean:css", function (cb) {
-    rimraf(paths.webroot + "css/", cb);
+gulp.task("clean:css", function(cb) {
+    rimraf(paths.css, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
-gulp.task("min:js",["typescript"], function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
+//gulp.task("min:js",["typescript"], function () {
+//    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+//        .pipe(concat(paths.concatJsDest))
+//        .pipe(uglify())
+//        .pipe(gulp.dest("."));
+//});
 
-gulp.task("min:css", function () {
-    return gulp.src([paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(cssmin())
-        .pipe(gulp.dest("."));
-});
+//gulp.task("min:css", function() {
+//    return gulp.src([paths.css, "!" + paths.minCss])
+//        .pipe(concat(paths.concatCssDest))
+//        .pipe(cssmin())
+//        .pipe(gulp.dest("."));
+//});
 
-gulp.task("min", ["typescript","sass"]);
+gulp.task("min", ["scripts", "sass"]);
 
 gulp.task("default", ["min"]);
 
-gulp.task("install", ["min"]);
+gulp.task("install", shell.task([
+        "\"node_modules/.bin/bower\" install",
+        "\"node_modules/.bin/gulp\""
+])
+);
 
-gulp.task("typescript", ["tsd:rebundle"] ,function () {
-    return gulp.src(paths.ts)
-        .pipe(ts({
-            noImplicitAny: true,
-            out: "site.js"
-        })
-        .pipe(gulp.dest(paths.webroot + "js/")));
+gulp.task("scripts",["tsd"], function () {
+    var tsResult = gulp.src(paths.ts)
+                       .pipe(sourcemaps.init()) // This means sourcemaps will be generated 
+                       .pipe(ts({
+                           sortOutput: true,
+                           noImplicitAny: true
+                       }));
+
+    return tsResult.js
+                .pipe(concat("site.js")) // You can use other plugins that also support gulp-sourcemaps 
+                .pipe(sourcemaps.write()) // Now the sourcemaps are added to the .js file 
+                .pipe(gulp.dest(paths.js));
 });
 
 gulp.task("tsd:install", shell.task([
         "\"node_modules/.bin/tsd\" install"
-])
+    ])
 );
 
-gulp.task("tsd:rebundle",["tsd:install"], shell.task([
+gulp.task("tsd:rebundle", ["tsd:install"], shell.task([
         "\"node_modules/.bin/tsd\" rebundle"
-])
+    ])
 );
 
-gulp.task("sass", function () {
-    return gulp.src(paths.webroot +"sass/*.scss")
+gulp.task("tsd", ["tsd:rebundle"]);
+
+gulp.task("sass", function() {
+    return gulp.src(paths.sass)
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass().on("error", sass.logError))
         .pipe(autoprefixer())
-        .pipe(concat("stylesheet.css"))
+        .pipe(concat("site.css"))
         .pipe(cssmin())
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest(paths.webroot + "css/"));
+        .pipe(gulp.dest(paths.css));
 });
