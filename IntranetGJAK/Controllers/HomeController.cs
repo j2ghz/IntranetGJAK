c411bef685b7015097dce72dd24a72bd4b561e6c
J2ghz <j2.00ghz@gmail.com>
2016-01-17 15:49:44 +0100

    add authorization

--- IntranetGJAK/Controllers/HomeController.cs
+++ IntranetGJAK/Controllers/HomeController.cs
@@ -9,34 +9,27 @@ namespace IntranetGJAK.Controllers
 {
     using IntranetGJAK.Models;
 
+    using Microsoft.AspNet.Authorization;
+
+    [Authorize]
     public class HomeController : Controller
     {
-        public HomeController()
-        {
-
-        }
 
         [FromServices]
         public ApplicationDbContext DbContext { get; set; }
-
+        [Authorize(Roles = "Write, Read")]
         public IActionResult Index()
         {
             return this.View(this.DbContext.Files);
         }
 
+        [Authorize(Roles = "Write")]
         public IActionResult Upload()
         {
-            if (this.User.Identity.IsAuthenticated)
-            {
-                return View();
-            }
-            else
-            {
-                return this.RedirectToAction("Login", "Account");
-            }
+            return View();
         }
-    
 
+        [AllowAnonymous]
         public IActionResult Error()
         {
             return this.View();
