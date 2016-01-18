fdc7ce3699224b7eafeb56a4b7df6d78aab08ffd
Jozef Hollý <j2.00ghz@gmail.com>
2016-01-18 12:48:42 +0100

    responsive tables

--- IntranetGJAK/wwwroot/js/index.js
+++ IntranetGJAK/wwwroot/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$("#filetable").DataTable({
-		"order": [[2, "desc"]],
+	    "order": [[2, "desc"]],
+	    responsive: true,
 		"language": {
 			"url": "//cdn.datatables.net/plug-ins/1.10.10/i18n/Czech.json"
 		}
