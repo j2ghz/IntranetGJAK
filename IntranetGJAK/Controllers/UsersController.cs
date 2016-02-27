d87f29a969344d8e2ecaddf72b2eb0c9c70e05c1
J2ghz <j2.00ghz@gmail.com>
2016-02-28 00:02:09 +0100

    Add UserController, Finish index view

---
+++ IntranetGJAK/Controllers/UsersController.cs
@@ -0,0 +1,102 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using IntranetGJAK.Models;
+using Microsoft.AspNet.Identity;
+
+namespace IntranetGJAK.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(UserManager<ApplicationUser> userManager, ApplicationDbContext applicationDbContext)
+        {
+            _userManager = userManager;
+            _context = applicationDbContext;
+        }
+
+        // GET: ApplicationUsers
+        public  IActionResult Index()
+        {
+            return View(_userManager);
+        }
+
+        // GET: ApplicationUsers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            ApplicationUser applicationUser = await _userManager.FindByIdAsync(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            ApplicationUser applicationUser = await _userManager.FindByIdAsync(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // POST: ApplicationUsers/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userManager.UpdateAsync(applicationUser);
+                return RedirectToAction("Index");
+            }
+            return View(applicationUser);
+        }
+
+        // GET: ApplicationUsers/Delete/5
+        [ActionName("Delete")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            ApplicationUser applicationUser = await _userManager.FindByIdAsync(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(applicationUser);
+        }
+
+        // POST: ApplicationUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            ApplicationUser applicationUser = await _userManager.FindByIdAsync(id);
+            await _userManager.DeleteAsync(applicationUser);
+            return RedirectToAction("Index");
+        }
+    }
+}
